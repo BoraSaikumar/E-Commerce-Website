3efd3a59fce562303b6acc43174e8643544c0b00
BoraSaikumar <136374511+BoraSaikumar@users.noreply.github.com>
2024-12-28 11:07:17 +0530

    Delete Ecommerce Firebase directory

--- Ecommerce Firebase/EcommerceFB/src/pages/noPage/NoPage.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const NoPage=()=> {
-  return (
-    <div>NoPage</div>
-  )
-}
-
-export default NoPage
--- Ecommerce Firebase/EcommerceFB/src/redux/selector.jsx
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectCartState = (state) => state.cart;
-
-export const selectCartItems = createSelector(
-  [selectCartState],
-  (cart) => cart.map(item => ({ ...item })) // Ensuring we are transforming the items for memoization purposes
-);
